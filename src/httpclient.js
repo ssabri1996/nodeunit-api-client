9e7e4355fd011cb2bf51a7f1b03fb41490cebfb2
sabri garrach <sabri.garrach@sofrecom.com>
2026-01-16 15:54:49 +0100

    feat: support https calls
    rollback to provided code on issue #https://github.com/powmedia/nodeunit-httpclient/pull/9

--- src/httpclient.js
+++ src/httpclient.js
@@ -1,214 +1,207 @@
-// src/httpclient.js
+'use strict';
+var querystring = require('querystring'),
+    underscore = require('underscore'),
+    debug;
 
-const http = require('http');
-const https = require('https');
-const querystring = require('querystring');
 
 /**
- * HttpClient
- *
- * @param {Object} options:
- *   - protocol ('http' | 'https')   : default 'http'
- *   - auth ('username:password')    : basic auth
- *   - host ('localhost')
- *   - port (80 / 443 par défaut selon protocol)
- *   - path ('')                     : base path (e.g. '/api')
- *   - reqHeaders ({})               : default request headers
- *   - headers ({})                  : default expected response headers
- *   - status (Number|null)          : default expected status code
- *   - timeout (Number)              : request timeout in ms (default 30000)
- *   - debug (Boolean)               : enable debug logs (default false)
+ * @param {object}  Options:
+ *                      auth ('username:password')
+ *                      host ('localhost')
+ *                      port (80)
+ *                      path ('')       - Base path URL e.g. '/api'
+ *                      headers ({})    - Test that these headers are present on every response (unless overridden)
+ *                      status (null)   - Test that every response has this status (unless overridden)
+ *						https (false) 	- https/http
+ * @param options
  */
-function HttpClient(options) {
-  options = options || {};
+var HttpClient = module.exports = function(options) {
+    options = options || {};
 
-  this.protocol = options.protocol || 'http';
-  this.auth = options.auth;
-  this.host = options.host || 'localhost';
-
-  if (typeof options.port !== 'undefined') {
-    this.port = options.port;
-  } else {
-    this.port = this.protocol === 'https' ? 443 : 80;
-  }
-
-  this.path = options.path || '';
-  this.reqHeaders = options.reqHeaders || {};
-  this.headers = options.headers || {};
-  this.status = options.status;
-  this.timeout = options.timeout || 30000;
-  this.debug = options.debug || false;
-
-  this.transport = this.protocol === 'https' ? https : http;
-}
-
-HttpClient.create = function(options) {
-  return new HttpClient(options);
+    this.auth = options.auth || undefined;
+    this.host = options.host || 'localhost';
+    this.port = options.port || 80;
+    this.path = options.path || '';
+    this.headers = options.headers || {};
+    this.status = options.status;
+    this.http = require(options.https ? 'https' : 'http');
+    debug = options.debug ? true : false;
 };
 
-const methods = ['get', 'post', 'head', 'put', 'del', 'trace', 'options', 'connect'];
+HttpClient.create = function(options) {
+    return new HttpClient(options);
+};
 
+var methods = ['get', 'post', 'head', 'put', 'del', 'trace', 'options', 'connect'];
+
+/**
+ * Performs a testable http request
+ *
+ * @param {Assert}
+ *   Nodeunit assert object
+ * @param {string} [route=undefined]
+ *   http uri to test
+ * @param {object} [req=undefined]
+ *   Object containing request related attributes like headers or body.
+ * @param {object} [res=undefined]
+ *   Object to compare with the response of the http call
+ * @param {Function} [cb=undefined]
+ *   Callback that will be called after the http call. Receives the http response object.
+ */
 methods.forEach(function(method) {
-  HttpClient.prototype[method] = function(assert, path, req, res, cb) {
-    const self = this;
+    HttpClient.prototype[method] = function(assert, path, req, res, cb) {
+        var self = this;
 
-    // Validation
-    if (typeof path !== 'string') {
-      throw new TypeError('path must be a string');
-    }
+        //Handle different signatures
+        if (arguments.length == 3) {
+            //(assert, path, cb)
+            if (typeof req === 'function') {
+                cb = req;
+                req = {};
+                res = {};
+            }
 
-    // Gestion des signatures
-    if (arguments.length === 3) {
-      if (typeof req === 'function') {
-        cb = req;
-        req = {};
-        res = {};
-      } else {
-        cb = null;
-        res = req;
-        req = {};
-      }
-    }
+            //(assert, path, res)
+            else {
+                cb = null;
+                res = req;
+                req = {};
+            }
+        }
 
-    if (arguments.length === 4) {
-      if (typeof res === 'function') {
-        cb = res;
-        res = {};
-      }
-    }
+        //(assert, path, req, cb)
+        if (arguments.length == 4) {
+            if (typeof res == 'function') {
+                cb = res;
+                res = {};
+            }
+        }
 
-    req = req || {};
-    res = res || {};
+        //Also accepted:
+        //(assert, path, req, res)
+        //(assert, path, req, res, cb)
 
-    // Construction du path
-    let fullPath = this.path + path;
+        //Generate path based on base path, route path and querystring params
+        var fullPath = this.path + path;
 
-    if (['post', 'put'].indexOf(method) === -1) {
-      const data = req.data;
-      if (data) {
-        fullPath += '?' + querystring.stringify(data);
-      }
-    }
+        //Don't add to querystring if POST or PUT
+        if (['post', 'put'].indexOf(method) === -1) {
+            var data = req.data;
 
-    const options = {
-      host: this.host,
-      port: this.port,
-      path: fullPath,
-      method: method === 'del' ? 'DELETE' : method.toUpperCase(),
-      headers: Object.assign({}, this.reqHeaders, req.headers)
-    };
+            if (data) {fullPath += '?' + querystring.stringify(data);}
+        }
 
-    if (req.auth) {
-      options.auth = req.auth;
-    } else if (this.auth) {
-      options.auth = this.auth;
-    }
+        var options = {
+            host: this.host,
+            port: this.port,
+            path: fullPath,
+            method: method == 'del' ? 'DELETE' : method.toUpperCase(),
+            headers: underscore.extend({}, this.reqHeaders, req.headers)
+        };
 
-    const request = this.transport.request(options);
+        if (req.auth) {
+            options.auth = req.auth;
+        } else if (this.auth) {
+            options.auth = this.auth;
+        }
 
-    // Timeout
-    request.setTimeout(this.timeout, function() {
-      request.abort();
-    });
+        var request = this.http.request(options);
 
-    // Gestion des erreurs
-    request.on('error', function(err) {
-      if (self.debug) {
-        console.error('HTTP request error:', {
-          message: err.message,
-          code: err.code,
-          host: options.host,
-          port: options.port,
-          path: options.path,
-          method: options.method
+        //Write POST & PUTdata
+        if (['post', 'put'].indexOf(method) != -1) {
+            var data = req.data || req.body;
+
+            if (data) {
+                if (typeof data == 'object') {
+                    request.setHeader('content-type', 'application/json');
+                    request.write(JSON.stringify(data));
+                } else {
+                    request.write(data);
+                }
+            }
+        }
+
+        if (debug) {httpClientLogger.log('REQUEST', request);}
+        //Send
+        request.end();
+
+        request.on('response', function(response) {
+            if (debug) {httpClientLogger.log('RESPONSE', response);}
+
+            response.setEncoding('utf8');
+
+            response.on('data', function(chunk) {
+                if (response.body) {response.body += chunk;} else {response.body = chunk;}
+            });
+
+            //Handle the response; run response tests and hand back control to test
+            response.on('end', function() {
+                //Add parsed JSON
+                var contentType = response.headers['content-type'];
+                if (contentType && contentType.indexOf('application/json') != -1) {
+                    if (typeof response.body != 'undefined') {
+                        //Catch errors on JSON.parse and attempt to handle cases where the response.body contains html
+                        try {
+                            response.data = JSON.parse(response.body);
+                        } catch (err) {
+                            console.log('JSON.parse response.body error:');
+                            console.log(err);
+                            if (debug) {httpClientLogger.log('RESPONSE.BODY', response.body);}
+                            var responseTest = response.body.split('{');
+                            if (responseTest.length > 1) {
+                                var actualResponse = '{' + responseTest[1];
+                                try {
+                                    response.data = JSON.parse(actualResponse);
+                                    console.log('JSON.parse second attempt success.');
+                                } catch (err) {
+                                    console.log('JSON.parse error on second parse attempt.');
+                                    console.log(err);
+                                    if (debug) {httpClientLogger.log('FILTERED RESPONSE.BODY', actualResponse);}
+                                }
+                            }
+                        }
+                    }
+                }
+
+                //Run tests on the response
+                (function testResponse() {
+                    //Can pass in falsy value to prevent running tests
+                    if (!assert) {return;}
+
+                    //Status code
+                    var status = res.status || self.status;
+                    if (status) {
+                        assert.equal(response.statusCode, status);
+                    }
+
+                    //Headers
+                    var headers = underscore.extend({}, self.headers, res.headers);
+                    for (var key in headers) {
+                        assert.equal(response.headers[key], headers[key]);
+                    }
+
+                    //Body
+                    if (res.body) {
+                        assert.equal(response.body, res.body);
+                    }
+
+                    //JSON data
+                    if (res.data) {
+                        assert.deepEqual(response.data, res.data);
+                    }
+                })();
+
+
+                //Done, return control to test
+                if (cb) {return cb(response);} else {return assert.done();}
+            });
         });
-      }
-
-      if (cb) {
-        return cb(err);
-      }
-
-      if (assert && typeof assert.ok === 'function' && typeof assert.done === 'function') {
-        assert.ok(false, 'HTTP request error: ' + err.message);
-        return assert.done();
-      }
-
-      // Sinon, on log sans crasher
-      console.error('HTTP request error (no handler):', err.message);
-    });
-
-    // Corps pour POST / PUT
-    if (['post', 'put'].indexOf(method) !== -1) {
-      const bodyData = req.data || req.body;
-
-      if (bodyData) {
-        if (typeof bodyData === 'object') {
-          request.setHeader('content-type', 'application/json');
-          request.write(JSON.stringify(bodyData));
-        } else {
-          request.write(bodyData);
-        }
-      }
-    }
-
-    // Envoi
-    request.end();
-
-    // Réponse
-    request.on('response', function(response) {
-      const contentType = response.headers['content-type'] || '';
-      
-      // Encoding UTF-8 seulement pour text/json
-      if (contentType.indexOf('text') !== -1 || contentType.indexOf('json') !== -1) {
-        response.setEncoding('utf8');
-      }
-
-      response.body = '';
-
-      response.on('data', function(chunk) {
-        response.body += chunk;
-      });
-
-      response.on('end', function() {
-        // Parse JSON
-        if (contentType.indexOf('application/json') !== -1 && response.body) {
-          try {
-            response.data = JSON.parse(response.body);
-          } catch (e) {
-            // JSON invalide
-          }
-        }
-
-        // Tests automatiques
-        if (assert) {
-          const expectedStatus = res.status || self.status;
-          if (expectedStatus) {
-            assert.equal(response.statusCode, expectedStatus);
-          }
-
-          const expectedHeaders = Object.assign({}, self.headers, res.headers);
-          for (const key in expectedHeaders) {
-            assert.equal(response.headers[key], expectedHeaders[key]);
-          }
-
-          if (res.body !== undefined) {
-            assert.equal(response.body, res.body);
-          }
-
-          if (res.data !== undefined) {
-            assert.deepEqual(response.data, res.data);
-          }
-        }
-
-        // Callback ou fin
-        if (cb) {
-          return cb(response);
-        } else if (assert && typeof assert.done === 'function') {
-          return assert.done();
-        }
-      });
-    });
-  };
+    };
 });
 
-module.exports = HttpClient;
+var httpClientLogger = {
+    log: function(header, data) {
+        console.log(header);
+        console.log(data);
+    }
+};
